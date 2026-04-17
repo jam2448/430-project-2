cbcd5bd090b12b45542b98b20e4580504175f322
Jarin Moses <jam2448@rit.edu>
2026-04-17 12:50:24 -0400

    working on making ingredient list

--- client/maker.jsx
+++ client/maker.jsx
@@ -6,24 +6,27 @@ const { useState, useEffect } = React;
 const { createRoot } = require('react-dom/client');
 
 
-const handleDomo = (e, onDomoAdded) => {
+const handleRecipe = (e, onRecipeAdded) => {
     e.preventDefault();
     helper.hideError();
 
-    const name = e.target.querySelector("#name").value;
-    const age = e.target.querySelector('#age').value;
-    const diet = e.target.querySelector('#diet').value;
+    const title = e.target.querySelector("#title").value;
+    const ingredients = e.target.querySelector('#ingredients').value;
+    const time = e.target.querySelector('#time').value;
+    const rating = e.target.querySelector('#rating').value;
 
-    if (!name || !age || !diet) {
-        helper.handleError("All make fields are required");
+
+    //if the user does not enter these fields, send an error
+    if (!title || !time || !rating || !ingredients) {
+        helper.handleError("Title, Time, Rating, and Ingredients are Required");
         return false;
     }
 
-    helper.sendPost(e.target.action, { name, age, diet }, onDomoAdded);
+    helper.sendPost(e.target.action, { title, ingredients, time, rating }, onRecipeAdded);
     return false;
 }
 
-const handleUpdate = (e, onDomoUpdated) => {
+const handleUpdate = (e, onRecipeUpdated) => {
 
     e.preventDefault();
     helper.hideError();
@@ -36,28 +39,56 @@ const handleUpdate = (e, onDomoUpdated) => {
         return false;
     }
 
-    helper.sendPost(e.target.action, { name, age }, onDomoUpdated);
+    helper.sendPost(e.target.action, { name, age }, onRecipeUpdated);
     return false;
 
+}
+
+//handles when the user modifies one of the ingredients so that it gets handles properly 
+const handleIngredientChange = (e, index) => {
+
+    //get the name of the ingredient to be modified
+    const ingName = e.target.ingredientName;
+
+    //create a copy of the ing. array to not directly modify the original
+    const newIngredients = [...ingredients];
+
+    newIngredients[index][ingName] = e.target.value;
+    setIngredients(newIngredients);
+
+};
+
+//Creates a new input field for the user to add a new ingredient to the list
+
+const AddIngredient = () => {
+    setIngredients([...ingredients, {ingredientName: ''}]);
 }
 
 
 
 const RecipeForm = (props) => {
+
+    {/* hook to handle the ingredient form when it is edited*/}
+    const [ingredients, setIngredients] = useState([{ingredientName: ''}]);
+
+
     return (
 
         <div>
 
+            {/* Setup the form. I want it to be a popup when the user clicks a plus icon
+                on the app. 
+            */}
             <Popup trigger={<button> Create Recipe </button>}
                 modal nested>
                 {
                     close => (
-                        <form id='domoForm'
-                            onSubmit={(e) => handleDomo(e, props.triggerReload)}
-                            name='domoForm'
+                        <form id='recipeForm'
+                            onSubmit={(e) => handleRecipe(e, props.triggerReload)}
+                            name='recipeForm'
                             action='/maker'
                             method='POST'
-                            className='domoForm'
+                            className='recipeForm'
                         >
                             <label htmlFor="title"> Recipe Name: </label>
                             <input id='title' type="text" name='title' placeholder='Enter Recipe Name...' />
@@ -68,10 +99,39 @@ const RecipeForm = (props) => {
                             <label htmlFor="rating">Rating: </label>
                             <input id="rating" type="number" name="age" min="0" max="10" />
 
+                            <label htmlFor="calories">Calories: </label>
+                            <input id="calories" type="text" name="calories" placeholder='Calorie Amount..' />
+
+                            <label htmlFor="servings">Servings: </label>
+                            <input id="servings" type="text" name="servings" placeholder='Servings' />
+
+                            {/* Dynamic ingredient form that allows the user to add 
+                                as many ingredients as they want.
+                                Referenced from: https://www.geeksforgeeks.org/reactjs/how-to-build-dynamic-forms-in-react/ */}
                             <label htmlFor="ingredients">Ingredients: </label>
 
+                                <form id="ingredients">
+                                    {ingredients.map((ingredient, index) =>(
+                                        
+                                        <div key={index}>
+                                            <input id="ingredientName" type="text" 
+                                            name="ingredientName" placeholder='List Ingredient' 
+                                            value={ingredient.ingredientName}
+                                            onChange={(e) => handleIngredientChange(e, index)}/>
+                                        </div>
+                                    ))}
 
-                            <input type="submit" className='makeDomoSubmit' value="Add Recipe" />
+                                    <button onClick={AddIngredient}>Add More Ingredient</button>
+
+                                </form>
+
+
+
+
+                            
+
+
+                            <input type="submit" className='makeRecipeSubmit' value="Add Recipe" />
                         </form>
                     )
                 }
@@ -89,19 +149,19 @@ const UpdateForm = (props) => {
 
     return (
 
-        <form action="/updateDomo"
+        <form action="/updateRecipe"
             name='updateForm'
-            className='domoForm'
+            className='recipeForm'
             method='POST'
             onSubmit={(e) => handleUpdate(e, props.triggerReload)}>
 
             <label htmlFor="name"> Name: </label>
-            <input id='name' type="text" name='name' placeholder='Domo Update Name' />
+            <input id='name' type="text" name='name' placeholder='Recipe Update Name' />
 
             <label htmlFor="age">Updated Age: </label>
             <input id="age" type="number" name="age" min="0" />
 
-            <input type="submit" className='makeDomoSubmit' value="Update Domo" />
+            <input type="submit" className='makeRecipeSubmit' value="Update Recipe" />
 
         </form>
 
@@ -109,43 +169,43 @@ const UpdateForm = (props) => {
     );
 }
 
-const DomoList = (props) => {
+const RecipeList = (props) => {
 
-    const [domos, setDomos] = useState(props.domos);
+    const [recipes, setRecipes] = useState(props.recipes);
 
     useEffect(() => {
-        const loadDomosFromServer = async () => {
-            const response = await fetch('/getDomos');
+        const loadRecipesFromServer = async () => {
+            const response = await fetch('/getRecipes');
             const data = await response.json();
-            setDomos(data.domos);
+            setRecipes(data.recipes);
         };
-        loadDomosFromServer();
-    }, [props.reloadDomos]);
+        loadRecipesFromServer();
+    }, [props.reloadRecipes]);
 
-    if (domos.length === 0) {
+    if (recipes.length === 0) {
 
         return (
-            <div className='domoList'>
-                <h3 className='emptyDomo'>No Domos Yet!</h3>
+            <div className='recipeList'>
+                <h3 className='emptyRecipe'>No Recipes Yet!</h3>
             </div>
         );
     }
 
-    const domoNodes = domos.map(domo => {
+    const recipeNodes = recipes.map(recipe => {
         return (
 
-            <div key={domo.id} className='domo'>
-                <img src="/assets/img/domoface.jpeg" alt="domo face" className='domoFace' />
-                <h3 className='domoName'> Name: {domo.name}</h3>
-                <h3 className='domoDiet'> Diet: {domo.diet}</h3>
-                <h3 className='domoAge'> Age: {domo.age}</h3>
+            <div key={recipe.id} className='recipe'>
+                <img src="/assets/img/recipeface.jpeg" alt="recipe face" className='recipeFace' />
+                <h3 className='recipeName'> Name: {recipe.name}</h3>
+                <h3 className='recipeDiet'> Diet: {recipe.diet}</h3>
+                <h3 className='recipeAge'> Age: {recipe.age}</h3>
             </div>
         );
     });
 
     return (
-        <div className='domoList'>
-            {domoNodes}
+        <div className='recipeList'>
+            {recipeNodes}
         </div>
     );
 }
@@ -153,23 +213,23 @@ const DomoList = (props) => {
 
 const App = () => {
 
-    const [reloadDomos, setReloadDomos] = useState(false);
+    const [reloadRecipes, setReloadRecipes] = useState(false);
 
     return (
 
         <div>
-            <div id='makeDomo'>
-                <DomoForm triggerReload={() => setReloadDomos(!reloadDomos)} />
+            <div id='makeRecipe'>
+                <RecipeForm triggerReload={() => setReloadRecipes(!reloadRecipes)} />
 
             </div>
-            <div id='updateDomo'>
+            <div id='updateRecipe'>
 
-                <UpdateForm triggerReload={() => setReloadDomos(!reloadDomos)} />
+                <UpdateForm triggerReload={() => setReloadRecipes(!reloadRecipes)} />
 
             </div>
 
-            <div id='domos'>
-                <DomoList domos={[]} reloadDomos={reloadDomos} />
+            <div id='recipes'>
+                <RecipeList recipes={[]} reloadRecipes={reloadRecipes} />
 
             </div>
         </div>
